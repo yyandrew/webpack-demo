6a478ec34b6ff74509e16e3374323c1304469985
andrew yang <andrew@ekohe.com>
2019-06-30 11:53:01 +0800

    Use Code Splitting

--- src/component.js
+++ src/component.js
@@ -5,6 +5,14 @@ export default (text = 'Hello World') => {
   const { first, second, ...others } = { first: 1, second: 2, third: 3, fourth: 4, fifth: 5 }
 
   element.innerHTML = `first: ${first}, second: ${second}`
+  element.onclick = () =>
+    import('./lazy')
+      .then(lazy => {
+        element.textContent = lazy.default
+      })
+      .catch(err => {
+        console.log(err)
+      })
 
   return element
 }
